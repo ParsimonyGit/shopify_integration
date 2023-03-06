2d1d63d818c64215e9211cada6d8ff0d74bd6907
Rohan Bansal <rohan@parsimony.com>
2023-03-06 16:39:11 +0530

    fix: create contact against customer

--- shopify_integration/customers.py
+++ shopify_integration/customers.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 
 import frappe
 from frappe import _
-from frappe.utils import cstr
+from frappe.utils import cstr, validate_phone_number
 
 if TYPE_CHECKING:
 	from erpnext.selling.doctype.customer.customer import Customer
@@ -39,6 +39,7 @@ def create_customer(shop_name: str, shopify_customer: "ShopifyCustomer"):
 				),
 				"territory": get_root_of("Territory"),
 				"customer_type": _("Individual"),
+				"exempt_from_sales_tax": shopify_customer.attributes.get("tax_exempt"),
 			}
 		)
 		customer.flags.ignore_mandatory = True
@@ -46,6 +47,7 @@ def create_customer(shop_name: str, shopify_customer: "ShopifyCustomer"):
 
 		if customer:
 			create_customer_address(customer, shopify_customer)
+			create_customer_contact(customer, shopify_customer)
 
 		frappe.db.commit()
 	except Exception as e:
@@ -62,7 +64,7 @@ def create_customer_address(customer: "Customer", shopify_customer: "ShopifyCust
 
 	address: "Address"
 	for index, address in enumerate(addresses):
-		frappe.get_doc(
+		address_doc = frappe.get_doc(
 			{
 				"doctype": "Address",
 				"shopify_address_id": address.id,
@@ -78,7 +80,40 @@ def create_customer_address(customer: "Customer", shopify_customer: "ShopifyCust
 				"email_id": shopify_customer.email,
 				"links": [{"link_doctype": "Customer", "link_name": customer.name}],
 			}
-		).insert(ignore_mandatory=True)
+		)
+		address_doc.insert(ignore_mandatory=True)
+
+
+def create_customer_contact(customer: "Customer", shopify_customer: "ShopifyCustomer"):
+	data = {
+		"status": "Passive",
+		"first_name": shopify_customer.attributes.get("first_name"),
+		"last_name": shopify_customer.attributes.get("last_name"),
+		"unsubscribed": not shopify_customer.attributes.get("accepts_marketing"),
+	}
+
+	if shopify_customer.attributes.get("email"):
+		data["email_ids"] = [
+			{"email_id": shopify_customer.attributes.get("email"), "is_primary": True}
+		]
+
+	phone_no = shopify_customer.attributes.get("phone")
+	if not phone_no:
+		default_address = shopify_customer.attributes.get("default_address")
+		if default_address:
+			phone_no = default_address.attributes.get("phone")
+
+	if phone_no and validate_phone_number(phone_no, throw=False):
+		data["phone_nos"] = [{"phone": phone_no, "is_primary_phone": True}]
+
+	contact = frappe.get_doc(
+		{
+			"doctype": "Contact",
+			**data,
+			"links": [{"link_doctype": "Customer", "link_name": customer.name}],
+		}
+	)
+	contact.insert(ignore_mandatory=True)
 
 
 def get_address_title(customer_name: str, index: int):
